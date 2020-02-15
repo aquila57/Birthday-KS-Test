c08edc3fca6b142e15e232024a4a3ef55fc55943
aquila57 <58740691+aquila57@users.noreply.github.com>
2020-02-15 10:17:18 -0600

    Add files via upload

--- deinst.sh
+++ deinst.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 make -f bdlib.mak clean
-make -f bday.mak clean
-make -f bd.mak clean
 make -f bdspc.mak clean
 make -f etausbd.mak clean
 make -f fibobd.mak clean
@@ -9,5 +7,4 @@ make -f lfsrbd.mak clean
 make -f randubd.mak clean
 make -f sinebd.mak clean
 make -f gslbd.mak clean
-make -f gen.mak clean
 rm -f allgsl.out
--- inst.sh
+++ inst.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 make -f bdlib.mak
-make -f bday.mak
-make -f bd.mak
 make -f bdspc.mak
 make -f etausbd.mak
 make -f fibobd.mak
@@ -9,4 +7,3 @@ make -f lfsrbd.mak
 make -f randubd.mak
 make -f sinebd.mak
 make -f gslbd.mak
-make -f gen.mak
